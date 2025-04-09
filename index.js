5f434d4639419fe4a9e5c9d71c7fe221da2877d5
sdefrancesco <skaterbastian@aol.com>
2025-04-08 22:27:45 -0400

    add view engine and two different views

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
 
 
-
-// app.get('/', (req, res)=> {
-//     res.render('/views/index')
-// })
+app.get('/', (req, res)=> {
+    res.render('index')
+})
+app.get('/about', (req, res)=> {
+    res.render('index')
+})
 
 
 
